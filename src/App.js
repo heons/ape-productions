83206c4c921b0a33f47735490b9e5f7a52b53b0c
HUEN OH <huen.oh@ssense.com>
2021-07-22 00:30:51 -0400

    update: auto video scaling based on the screen size

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import SampleVideoFrame from './components/SampleVideoFrame';
+import useWindowDimensions from './components/useWindowDimensions'
 
 function App() {
+  const { height, width } = useWindowDimensions();
+
+  // console.log(height, width);
   return (
     <div className="App">
-      <SampleVideoFrame src="video/mov_bbb.mp4"/>
+      <SampleVideoFrame
+        pos={{x:10, y:10}}
+        screenSize={{height, width}}
+        src="video/mov_bbb.mp4"
+      />
+      <SampleVideoFrame
+        pos={{x:200, y:180}}
+        screenSize={{height, width}}
+        src="video/mov_bbb.mp4"
+      />
     </div>
   );
 }
--- src/components/SampleVideoFrame.js
+++ src/components/SampleVideoFrame.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
-const SampleVideoFrame = (props) => {
+const computeSizeFromWidth = (size, width) => {
+  const ratio = width / 800;
+  return {
+    width: Math.round(size.width * ratio),
+    height: Math.round(size.height * ratio)
+  }
+}
+
+const SampleVideoFrame = ({ src, screenSize, pos, isMouseOver }) => {
+  // const [naturalSize, setNaturalSize] = useState({width: 300, height: 200});
+  const [naturalSize, setNaturalSize] = useState({width: 100, height: 100});
+
+  const styles = {
+    opacity: isMouseOver ? 1 : 0.5,
+    position: 'absolute',
+    top: pos.y,    // computed based on child and parent's height
+    left: pos.x   // computed based on child and parent's width
+  };
+
+  // useEffect(() => {
+  //   setSize(computeSizeFromWidth(size, screenSize.width));
+  // }, [screenSize]);
+  
+  const size = computeSizeFromWidth(naturalSize, screenSize.width);
+
   return (
     <div>
-      <video src={props.src} style={{opacity: 0.5}} preload="auto" loop muted autoPlay playsInline>
+      <video
+        src={src}
+        width={size.width}
+        height={size.height}
+        style={styles}
+        preload="auto" loop muted autoPlay playsInline
+        onLoadedData={response => {
+          const { videoWidth, videoHeight } = response.target;      
+          setNaturalSize({width: videoWidth, height: videoHeight});
+        }}
+      >
         Your browser does not support the HTML5 Video element.
       </video>
     </div>
