27fbd6ecaa996ee24bf432487083cd028c9ec92a
HUEN OH <huen.oh@ssense.com>
2021-07-22 21:47:22 -0400

    add useMousePosition hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import SampleVideoFrame from './components/SampleVideoFrame';
 import useWindowDimensions from './hooks/useWindowDimensions'
+import useMousePosition from './hooks/useMousePosition';
+
 
 function App() {
   const { height, width } = useWindowDimensions();
+  const { x: mX, y: mY } = useMousePosition();
+
+  const hasMovedCursor = typeof mX === "number" && typeof mY === "number";
 
   // console.log(height, width);
+  // console.log(mX, mY);
   return (
     <div className="App">
+      <h1>
+        {hasMovedCursor
+          ? `Your cursor is at ${mX}, ${mY}.`
+          : "Move your mouse around."}
+      </h1>
       <SampleVideoFrame
         pos={{x:10, y:10}}
         screenSize={{height, width}}
