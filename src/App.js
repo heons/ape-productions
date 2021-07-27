d7e85e7eea712fe5f1d165ad9d0a5e5b6280711d
HUEN OH <huen.oh@ssense.com>
2021-07-26 23:36:15 -0400

    update: movement - working version 1

--- src/App.js
+++ src/App.js
@@ -2,82 +2,28 @@ import './App.css';
 import SampleVideoFrame from './components/SampleVideoFrame';
 import useWindowDimensions from './hooks/useWindowDimensions'
 import useMousePosition from './hooks/useMousePosition';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useRef } from 'react';
 
 
 function App() {
   const { height, width } = useWindowDimensions();
-  const { mousePosition, mouseSpeed }= useMousePosition();
-  console.log(mouseSpeed);
+  const { mousePosition } = useMousePosition();
 
-  const speedGroup1 = useRef({x: 0, y: 0});
-  const speedGroup2 = useRef({x: 0, y: 0});
-  const isMouseLeftOnScreen = useRef(width/2 > mousePosition.x);
-
-  const [counters, setCounters] = useState(0);
+  const targetPos1 = useRef({x: 0, y: 0});
+  const targetPos2 = useRef({x: 0, y: 0});
 
   const hasMovedCursor = typeof mousePosition.x === "number" && typeof mousePosition.y === "number";
 
-  isMouseLeftOnScreen.current = width/2 > mousePosition.x;
-  // console.log(isMouseLeftOnScreen);
-  // useEffect(() => {
-  //   // const speedRatio = 50;
-  //   // const moveSpeedX = (width/2 - mousePosition.x) / speedRatio;
-  //   // const moveSpeedY = (height/2 - mousePosition.y) / speedRatio;
-  //   // const calcSpeed = { x: moveSpeedX, y: moveSpeedY };
-  //   // console.log(calcSpeed);
-  //   let moveTimer = setInterval(() => {
-     
-  //     setSeconds(seconds => seconds + 1);
-  //     // setSpeed(mousePosition => ({x: mousePosition.x + 1, y: mousePosition.y + 1}));
-  //     // setSpeed(speedGroup1 => ({x: speedGroup1.x + 1, y: speedGroup1.y + 1}));
-  //     // console.log(seconds);
-  //   }, 1000);
-  //   return () => clearInterval(moveTimer);
-  // }, []);
 
-  useEffect(() => {
-    let moveTimer = setInterval(() => {
-      // setSpeed(mousePosition => ({x: mousePosition.x + 1, y: mousePosition.y + 1}));
-      // setSpeed(speedGroup1 => ({x: speedGroup1.x + 1, y: speedGroup1.y + 1}));
-      // console.log(seconds);
-      setCounters(counters => counters + 1);
-    }, 1000);
-    return () => clearInterval(moveTimer);
-  }, []);
-
-  const minLimit = -100;
-  const maxLimit = 100;
-
-  const speedRatio = 1000;
-  const moveSpeedX = (width/2 - mousePosition.x) / speedRatio;
-  const moveSpeedY = (height/2 - mousePosition.y) / speedRatio;
-  const calcSpeed = { x: moveSpeedX, y: moveSpeedY };
-
-  // speedGroup1.current = {
-  //   x: Math.max(Math.min(speedGroup1.current.x + calcSpeed.x, maxLimit), minLimit),
-  //   y: Math.max(Math.min(speedGroup1.current.y + calcSpeed.y, maxLimit), minLimit),
-  // };
-
-  speedGroup1.current = {
-    x: Math.max(Math.min(speedGroup1.current.x + mouseSpeed.x/100, maxLimit), minLimit),
-    y: Math.max(Math.min(speedGroup1.current.y + mouseSpeed.y/100, maxLimit), minLimit),
+  targetPos1.current = {
+    x: -(mousePosition.x - width/2),
+    y: -(mousePosition.y - height/2)
   };
 
-  speedGroup2.current = {
-    x: Math.max(Math.min(speedGroup2.current.x + mouseSpeed.x/50, maxLimit), minLimit),
-    y: Math.max(Math.min(speedGroup2.current.y + mouseSpeed.y/50, maxLimit), minLimit),
+  targetPos2.current = {
+    x: -(mousePosition.x - width/2) + width/50,
+    y: -(mousePosition.y - height/2) + height/50
   };
-
-  // if (isMouseLeftOnScreen) {
-  //   speedGroup1.current = {
-  //     x: Math.max(Math.min(speedGroup1.current.x -1, maxLimit), minLimit),
-  //     y: Math.max(Math.min(speedGroup1.current.y, maxLimit), minLimit),
-  //   };
-  // }
-      
-  // console.log(speedGroup1.current);
-  // console.log(height, width);
 
   return (
     <div className="App">
@@ -87,16 +33,17 @@ function App() {
           : "Move your mouse around."}
       </h1>
       <SampleVideoFrame
-        initPos={{x:10, y:10}}
-        screenSize={{height, width}}
-        addPos={speedGroup1.current}
         src="video/mov_bbb.mp4"
+        screenSize={{height, width}}
+        initPos={{x:100, y:100}}
+        targetPos={targetPos1.current}
       />
+
       <SampleVideoFrame
-        initPos={{x:200, y:180}}
-        screenSize={{height, width}}
-        addPos={speedGroup2.current}
         src="video/mov_bbb.mp4"
+        screenSize={{height, width}}
+        initPos={{x:200, y:300}}
+        targetPos={targetPos2.current}
       />
     </div>
   );
--- src/components/SampleVideoFrame.js
+++ src/components/SampleVideoFrame.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
+import styled from 'styled-components';
+
 
 const computeSizeFromWidth = (size, width) => {
   const ratio = width / 800;
@@ -10,42 +12,53 @@ const computeSizeFromWidth = (size, width) => {
   }
 }
 
-const SampleVideoFrame = ({ src, screenSize, initPos, addPos }) => {
+const MoveDivWrap = styled.div`
+  position: absolute;
+  transition: all 3s linear;
+  transform: translate(${props => props.endPos.x}px, ${props => props.endPos.y}px);
+`;
+
+const SampleVideoFrame = ({ src, screenSize, initPos, targetPos }) => {
   // const [naturalSize, setNaturalSize] = useState({width: 300, height: 200});
   const [naturalSize, setNaturalSize] = useState({width: 100, height: 100});
   const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const newTargetPos = useRef({...initPos});
 
   const size = computeSizeFromWidth(naturalSize, screenSize.width);
 
   const styles = {
     opacity: isMouseOver ? 1 : 0.5,
-    position: 'absolute',
-    top: Math.round(initPos.y * size.ratio + addPos.y),    // computed based on child and parent's height
-    left: Math.round(initPos.x * size.ratio + addPos.x)   // computed based on child and parent's width
   };
 
-  // useEffect(() => {
-  //   setSize(computeSizeFromWidth(size, screenSize.width));
-  // }, [screenSize]);
-
-
+  // console.log(targetPos)
+  const ACT_RESOLUTION = 10;
+  if(Math.round(targetPos.x) % ACT_RESOLUTION === 0) {
+    newTargetPos.current = {
+      x: targetPos.x + initPos.x,
+      y: targetPos.y + initPos.y,
+    };
+  }
+  
   return (
     <div>
-      <video
-        src={src}
-        width={size.width}
-        height={size.height}
-        style={styles}
-        preload="auto" loop muted autoPlay playsInline
-        onLoadedData={response => {
-          const { videoWidth, videoHeight } = response.target;      
-          setNaturalSize({width: videoWidth, height: videoHeight});
-        }}
-        onMouseOver={() => setIsMouseOver(true)}
-        onMouseLeave={() => setIsMouseOver(false)}
-      >
-        Your browser does not support the HTML5 Video element.
-      </video>
+      <MoveDivWrap endPos={newTargetPos.current}>
+        <video
+          src={src}
+          width={size.width}
+          height={size.height}
+          style={styles}
+          preload="auto" loop muted autoPlay playsInline
+          onLoadedData={response => {
+            const { videoWidth, videoHeight } = response.target;      
+            setNaturalSize({width: videoWidth, height: videoHeight});
+          }}
+          onMouseOver={() => setIsMouseOver(true)}
+          onMouseLeave={() => setIsMouseOver(false)}
+        >
+          Your browser does not support the HTML5 Video element.
+        </video>
+      </MoveDivWrap>
     </div>
   )
 }
