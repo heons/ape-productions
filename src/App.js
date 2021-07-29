9d8b872328fc49e3910c515a0efc75086744533e
HUEN OH <huen.oh@ssense.com>
2021-07-28 21:39:25 -0400

    add initial ArtistDetail page with route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { Route } from 'react-router-dom'
 import Home from './components/Home'
-
+import ArtistDetail from './components/ArtistDetail'
 
 function App() {
     return (
         <div className="App">
             <Route path="/" component={Home} exact={true}/>
+            <Route path="/:name" component={ArtistDetail}/>
         </div>
     );
 }
---
+++ src/components/ArtistDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ArtistDetail = (props) =>{
+    return (
+        <div>
+            ArtistDetail Page - Working on it.
+        </div>
+    )
+}
+
+export default ArtistDetail
+
