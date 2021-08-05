a6b7fe7a7793328aa72e14e925cdef4375c3d014
HUEN OH <huen.oh@ssense.com>
2021-08-05 08:00:55 -0400

    adjust size of video in artist detail page

--- src/components/ArtistDetail.js
+++ src/components/ArtistDetail.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { getArtistDetail } from '../resource'
+import useWindowDimensions from '../hooks/useWindowDimensions'
+
 
 const ArtistDetail = ({ match }) =>{
 
@@ -7,19 +9,21 @@ const ArtistDetail = ({ match }) =>{
     console.log(artist);
     console.log(artist.subItems[0].url);
 
+    const { height, width } = useWindowDimensions();
+    const ratio = 7 / 10;
+
     return (
         <div>
             <h1>{artist.title}</h1>
             <iframe
                 title={artist.title}
                 src={artist.subItems[0].url}
-                width="960"
-                height="540"
+                width={width * ratio}
+                height={height * ratio}
                 frameBorder="0"
                 allow="autoplay; fullscreen; picture-in-picture"
                 allowFullScreen>
             </iframe>
-
         </div>
     )
 }
