c0e538ac1fc8a26dfd59daccf338927d8afa42e3
HUEN OH <huen.oh@ssense.com>
2021-08-06 21:55:21 -0400

    add initial menu component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route } from 'react-router-dom'
 import Home from './components/Home'
+import Menu from './components/Menu'
 import ArtistDetail from './components/ArtistDetail'
 
 function App() {
     return (
         <div className="App">
+            <Menu/>
             <Route path="/" component={Home} exact={true}/>
             <Route path="/:title" component={ArtistDetail}/>
         </div>
---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import './Menu.css';
+import React from 'react'
+
+const Menu = (props) => {
+
+    return (
+        <div className='Menu'>
+            <h1>APE.</h1>
+            <h1>FILM / PHOTO / CLIENT / NOTE / CONTACT</h1>
+        </div>
+    )
+}
+
+export default Menu;
