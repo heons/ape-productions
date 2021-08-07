dfcad45c05b8e6a2af32889d7ab2bd1fb10fc8fb
HUEN OH <huen.oh@ssense.com>
2021-08-06 23:11:11 -0400

    update menu component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import { useState, useCallback } from 'react';
 import { Route } from 'react-router-dom'
 import Home from './components/Home'
 import Menu from './components/Menu'
 import ArtistDetail from './components/ArtistDetail'
 
 function App() {
+
+    const [category, setCategory] = useState(false);
+    const onSelectCategory = useCallback((item) => {
+        setCategory(item);
+    }, [setCategory]);
+
     return (
         <div className="App">
-            <Menu/>
+            <Menu category={category} onSelect={onSelectCategory}/>
             <Route path="/" component={Home} exact={true}/>
             <Route path="/:title" component={ArtistDetail}/>
         </div>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,41 @@
 import './Menu.css';
-import React from 'react'
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
 
-const Menu = (props) => {
+const Menu = ({ category, onSelect }) => {
+
+    const items = useRef(
+        [
+            'Film',
+            'Photo',
+            'Client',
+            'Note',
+            'Contact',
+        ]
+    );
 
     return (
         <div className='Menu'>
-            <h1>APE.</h1>
-            <h1>FILM / PHOTO / CLIENT / NOTE / CONTACT</h1>
+            <h1>
+                <Link to={'/'} className='Menu-item' onClick={() => onSelect('item')}>
+                    APE.
+                </Link>
+            </h1>
+            <h1>
+                {'/ '}
+                {items.current.map((item) => {
+                    return (
+                        <Link 
+                            to={'/?' + item.toLowerCase()}
+                            key={item}
+                            className={category === item ? 'Menu-item-selected' : 'Menu-item'}
+                            onClick={() => onSelect(item)}
+                        >
+                            {item + ' / '}
+                        </Link>
+                    )
+                })}
+            </h1>
         </div>
     )
 }
