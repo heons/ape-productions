00c5b949dbda55f6e5963db8fddb0b1f46e75c64
HUEN OH <huen.oh@ssense.com>
2021-08-27 17:11:36 -0400

    update PhotoList to be responsive repect to screen ratio

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
+import { getBasePageStyle } from '../utils'
+
+// Resources
 import { photoWorkList, getPhotoUrlsByIndex } from '../resources/photos'
+
+// Hooks
+import useWindowDimensions from '../hooks/useWindowDimensions'
 
 
 const PhotoList = ({ match }) =>{
+    const { height, width } = useWindowDimensions();
 
     const workName = photoWorkList[match.params.id].name;
     const photoUrls = getPhotoUrlsByIndex(match.params.id);
 
+    const isHorizontal = (width > height);
+
+    const pageStyle = {
+        ...getBasePageStyle(width, height, 1),
+        backgroundColor: 'black',
+        overflowX: isHorizontal ? 'scroll' : null,
+        overflowY: !isHorizontal ? 'scroll' : null,
+    };
+
+    const itemStyle = {
+        height: isHorizontal ? '100vh' : null,
+        width: !isHorizontal ? '100vw' : null,
+        border: '0',
+        textAlign: 'center',
+    }
+
     return (
-        <div style={{'overflowX': 'scroll'}}>
-            <ListGroup horizontal>
+        <div style={pageStyle}>
+            <ListGroup horizontal={isHorizontal ? true : null}>
                 {
                     photoUrls.map((url, idx) => {
                         return (
                             <ListGroup.Item
                                 key={`${workName}-${idx}`}
-                                style={{'height': '100vh', 'border': '0', 'textAlign': 'center'}}
+                                style={itemStyle}
                             >
-                                <img height='100%' style={{'padding': '0'}} src={`${url}`} alt={`${workName}-${idx}`}></img>
+                                <img 
+                                    src={`${url}`}
+                                    alt={`${workName}-${idx}`}
+                                    style={{'padding': '0'}}
+                                    height={isHorizontal ? '100%': null}
+                                    width={!isHorizontal ? '100%': null}
+                                ></img>
                             </ListGroup.Item>
                     )})
                 }
