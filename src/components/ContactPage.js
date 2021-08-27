9e46c78c194e1de55331991a26ac0b3ee2c5cbd4
HUEN OH <huen.oh@ssense.com>
2021-08-26 22:45:44 -0400

    adjust font size and pos in contact page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -8,7 +8,7 @@ const ContactPage = ({ width, height, zIndex }) => {
         position: 'absolute',
         top: `0px`,
         left: `0px`,
-        paddingTop: `100px`,
+        paddingTop: `120px`,
         paddingLeft: `30px`,
         // backgroundColor: 'black',
         zIndex: zIndex,
@@ -21,10 +21,10 @@ const ContactPage = ({ width, height, zIndex }) => {
             <div
                 style={{color: 'white', textAlign: 'left'}}
             >
-                <h2>서울특별시 마포구 양화진길 6, 6층</h2>
-                <h2>+82-10-8487-2110</h2>
-                <h2>+82-10-4642-8321</h2>
-                <h2>apeproductionskr@gmail.com</h2>
+                <h1>서울특별시 마포구 양화진길 6, 6층</h1>
+                <h1>+82-10-8487-2110</h1>
+                <h1>+82-10-4642-8321</h1>
+                <h1>apeproductionskr@gmail.com</h1>
             </div>
         </div>
     )
