61b6c6f5c7855793eb4d6d79f995dfce6de24d26
HUEN OH <huen.oh@ssense.com>
2021-09-09 21:15:20 -0400

    update CaontactPage with Row Col

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import { getBasePageStyle } from '../utils'
-
-// Hooks
-import useWindowDimensions from '../hooks/useWindowDimensions'
+import { Row, Col } from 'react-bootstrap';
 
 
 const ContactPage = ({ zIndex }) => {
-    const { height, width } = useWindowDimensions();
-
     const pageStyle = {
-        ...getBasePageStyle(width, height, zIndex),
-        paddingLeft: `30px`,
+        position: 'absolute',
+        top: '0px',
+        left: '0px',
+        zIndex: zIndex,
     };
     
     return (
@@ -18,12 +15,20 @@ const ContactPage = ({ zIndex }) => {
             className='PagePaddingTop'
             style={pageStyle}
         >
-            <div>
-                <h1>서울특별시 마포구 양화진길 6, 6층</h1>
-                <h1>+82-10-8487-2110</h1>
-                <h1>+82-10-4642-8321</h1>
-                <h1>apeproductionskr@gmail.com</h1>
-            </div>
+            <Row>
+                <Col xs={12}>
+                    <h1>서울특별시 마포구 양화진길 6, 6층</h1>
+                </Col>
+                <Col xs={12}>
+                    <h1>+82-10-8487-2110</h1>
+                </Col>
+                <Col xs={12}>
+                    <h1>+82-10-4642-8321</h1>
+                </Col>
+                <Col xs={12}>
+                    <h1>apeproductionskr@gmail.com</h1>
+                </Col>
+            </Row>
         </div>
     )
 }
