5703dbdd5c0c20114bf5eae9a87ea78621b89d90
HUEN OH <huen.oh@ssense.com>
2021-10-02 18:47:55 -0400

    format App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
 import { useEffect } from 'react';
-import { Route, withRouter } from 'react-router-dom'
-import Home from './components/Home'
-import Menu from './components/Menu'
-import ArtistDetail from './components/ArtistDetail'
-import PhotoList from './components/PhotoList'
-import qs from 'qs'
+import { Route, withRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import ArtistDetail from './components/ArtistDetail';
+import PhotoList from './components/PhotoList';
+import qs from 'qs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactGA from 'react-ga';
 
-
-function App({location, history}) {
+function App({ location }) {
     const query = qs.parse(location.search, { ignoreQueryPrefix: true });
 
+    // GA Practice.
     useEffect(() => {
         ReactGA.initialize('UA-208149030-1');
         // ReactGA.set({ dimension1: 'UserGroup1' });
         ReactGA.ga('set', 'dimension1', 'UserGroup1');
         ReactGA.pageview(window.location.pathname + window.location.search);
     }, []);
-
     // ReactGA.ga('send', 'event', 'Video', 'play', 'cats.mp4', { dimension1: 'UserGroup1' });
 
     return (
@@ -28,17 +27,16 @@ function App({location, history}) {
             style = {{"border": "1px solid black"}}>
                 {}
             </canvas> */}
-            {/* <Menu category={category ? category : categoryFromUrl} onSelect={onSelectCategory}/> */}
             <Menu category={query.category} />
             <Route
                 path={`/`}
                 exact={true}
                 render={(props) => (
-                    <Home {...props} category={query.category}/>
+                    <Home {...props} category={query.category} />
                 )}
             />
-            <Route path={`/:id`} component={ArtistDetail} exact={true}/>
-            <Route path={`/photo/:id`} component={PhotoList}/>
+            <Route path={`/:id`} component={ArtistDetail} exact={true} />
+            <Route path={`/photo/:id`} component={PhotoList} />
         </div>
     );
 }
