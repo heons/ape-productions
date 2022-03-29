e7c611123bcb895d23fdd6d4e3f45cd64e293e5a
HUEN_OH <heons921@gmail.com>
2022-03-28 23:15:58 -0400

    change text size in ContactPage

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -21,13 +21,13 @@ const ContactPage = ({ zIndex }) => {
         <div className="PagePaddingTop" style={pageStyle}>
             <Row>
                 <Col xs={12}>
-                    <h1>서울특별시 마포구 양화진길 6, 6층</h1>
+                    <h3>서울특별시 마포구 양화진길 6, 6층</h3>
                 </Col>
                 <Col xs={12}>
-                    <h1>+82-10-8487-2110</h1>
+                    <h3>+82-10-8487-2110</h3>
                 </Col>
                 <Col xs={12}>
-                    <h1>oldboyung@gmail.com</h1>
+                    <h3>oldboyung@gmail.com</h3>
                 </Col>
                 <Col xs={12}>
                     <a
