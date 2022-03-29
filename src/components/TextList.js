ae9f35abe255f2fb87740eecc3022811c3d121fa
HUEN_OH <heons921@gmail.com>
2022-03-28 22:30:08 -0400

    remove interactive text styling in ClientPage

--- src/components/TextList.js
+++ src/components/TextList.js
@@ -1,9 +1,9 @@
 import './TextList.css';
-import React, { useState }from 'react'
+import React from 'react'
 
 const TextList = ({ textList, width, height }) =>{
     
-    const [idxMouseOver, setIdxMouseOver] = useState(0)
+    // const [idxMouseOver, setIdxMouseOver] = React.useState(0)
 
     const styleNoraml = {
         'fontSize': '100%',
@@ -11,13 +11,13 @@ const TextList = ({ textList, width, height }) =>{
         'margin': '0px',
         'padding': '0px',
     };
-    const styleMouseOver = {
-        'fontSize': '130%',
-        'color': 'white',
-        'margin': '0px',
-        'padding': '0px',
-        'border': '1px solid #ddd',
-    };
+    // const styleMouseOver = {
+    //     'fontSize': '130%',
+    //     'color': 'white',
+    //     'margin': '0px',
+    //     'padding': '0px',
+    //     'border': '1px solid #ddd',
+    // };
 
     return (
         <div>
@@ -31,8 +31,9 @@ const TextList = ({ textList, width, height }) =>{
                 {textList.map((company, idx) => (
                     <h4
                         key={company+idx}
-                        style={idx === idxMouseOver ? styleMouseOver : styleNoraml}
-                        onMouseOver={() => setIdxMouseOver(idx)}
+                        style={styleNoraml}
+                        // style={idx === idxMouseOver ? styleMouseOver : styleNoraml}
+                        // onMouseOver={() => setIdxMouseOver(idx)}
                     >
                         {company}
                     </h4>
