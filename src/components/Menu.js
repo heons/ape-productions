c061a6e433f6b376bfa258756c2a10ff76c75c10
HUEN_OH <heons921@gmail.com>
2022-12-24 21:15:40 -0500

    update size of main title to 30p

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,115 +1,136 @@
-import './Menu.css';
-import React, { useState } from 'react'
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
-import { LinkContainer } from 'react-router-bootstrap';
+import "./Menu.css";
+import React, { useState } from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 // Resources
-import { artistInfos } from '../resources/films'
+import { artistInfos } from "../resources/films";
 
 // Hooks
-import useWindowDimensions from '../hooks/useWindowDimensions'
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
 const Menu = ({ category, pathname }) => {
-    const { width } = useWindowDimensions();
-    const [expanded, setExpanded] = useState(false);
-    const nameStyle = {
-        marginLeft: '5px',
-        padding: '0px'
-    };
+  const { width } = useWindowDimensions();
+  const [expanded, setExpanded] = useState(false);
+  const nameStyle = {
+    marginLeft: "5px",
+    padding: "0px",
+  };
 
-    const titleOnTop = width >= 767 &&
+  const titleOnTop = width >= 767 && (
+    <Container>
+      <Link
+        to="/"
+        onClick={() => {
+          setExpanded(false);
+        }}
+        style={nameStyle}
+      >
+        <div className="title">OH EUN HO</div>
+      </Link>
+    </Container>
+  );
+
+  const titleInNavBar = width < 767 && (
+    <LinkContainer
+      to="/"
+      onClick={() => {
+        setExpanded(false);
+      }}
+    >
+      <Navbar.Brand>OH EUN HO</Navbar.Brand>
+    </LinkContainer>
+  );
+
+  const artistInfosMenu = artistInfos.filter(
+    (info) => info.subItems.length > 0
+  );
+
+  return (
+    <div className="MenuM">
+      {titleOnTop}
+      <Navbar variant="dark" expand="md" expanded={expanded}>
         <Container>
-            <Link
-                to='/'
-                onClick={() => { setExpanded(false) }}
-                style={nameStyle}
-            >
-                OH EUN HO
-            </Link>
+          {titleInNavBar}
+          <Navbar.Toggle
+            onClick={() => setExpanded(expanded ? false : "expanded")}
+          />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown title="FILM" className="dropdown-film">
+                {artistInfosMenu.map((artist, index) => {
+                  return (
+                    <LinkContainer
+                      key={`film-${index}`}
+                      to={`/${artist.id}`}
+                      onClick={() => {
+                        setExpanded(false);
+                      }}
+                    >
+                      <NavDropdown.Item eventKey={`film-${index}`}>
+                        {artist.title}
+                      </NavDropdown.Item>
+                    </LinkContainer>
+                  );
+                })}
+              </NavDropdown>
+              <Nav.Link
+                as={Link}
+                eventKey="photo-1"
+                to={pathname === "/photo/0" ? `/` : `/photo/0`}
+                onClick={() => {
+                  setExpanded(false);
+                }}
+              >
+                PHOTO
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                eventKey="dvd-1"
+                to={category === "dvd" ? `/` : `/?category=${"dvd"}`}
+                onClick={() => {
+                  setExpanded(false);
+                }}
+              >
+                DVD
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                eventKey="note-1"
+                to={category === "note" ? `/` : `/?category=${"note"}`}
+                onClick={() => {
+                  setExpanded(false);
+                }}
+              >
+                NOTE
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                eventKey="client-1"
+                to={category === "client" ? `/` : `/?category=${"client"}`}
+                onClick={() => {
+                  setExpanded(false);
+                }}
+              >
+                CLIENT
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                eventKey="contact-1"
+                to={category === "contact" ? `/` : `/?category=${"contact"}`}
+                onClick={() => {
+                  setExpanded(false);
+                }}
+              >
+                INFO
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-
-    const titleInNavBar = width < 767 &&
-        <LinkContainer to='/' onClick={() => { setExpanded(false) }}>
-            <Navbar.Brand>
-                OH EUN HO
-            </Navbar.Brand>
-        </LinkContainer>
-
-    const artistInfosMenu = artistInfos.filter(info => info.subItems.length > 0);
-
-    return (
-        <div className='MenuM'>
-            {titleOnTop}
-            <Navbar variant="dark" expand="md"
-                expanded={expanded}
-            >
-                <Container>
-                    {titleInNavBar}
-                    <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <NavDropdown title="FILM" className="dropdown-film">
-                                {artistInfosMenu.map((artist, index) => {
-                                    return (
-                                        <LinkContainer
-                                            key={`film-${index}`}
-                                            to={`/${artist.id}`}
-                                            onClick={() => { setExpanded(false) }}
-                                        >
-                                            <NavDropdown.Item eventKey={`film-${index}`}>
-                                                {artist.title}
-                                            </NavDropdown.Item>
-                                        </LinkContainer>
-                                    )
-                                })}
-                            </NavDropdown>
-                            <Nav.Link
-                                as={Link}
-                                eventKey="photo-1"
-                                to={pathname === '/photo/0' ? `/` : `/photo/0`}
-                                onClick={() => { setExpanded(false) }}
-                            >
-                                PHOTO
-                            </Nav.Link>
-                            <Nav.Link
-                                as={Link}
-                                eventKey="dvd-1"
-                                to={category === 'dvd' ? `/` : `/?category=${'dvd'}`}
-                                onClick={() => { setExpanded(false) }}
-                            >
-                                DVD
-                            </Nav.Link>
-                            <Nav.Link
-                                as={Link}
-                                eventKey="note-1"
-                                to={category === 'note' ? `/` : `/?category=${'note'}`}
-                                onClick={() => { setExpanded(false) }}   
-                            >
-                                NOTE
-                            </Nav.Link>
-                            <Nav.Link 
-                                as={Link}
-                                eventKey="client-1"
-                                to={category === 'client' ? `/` : `/?category=${'client'}`}
-                                onClick={() => { setExpanded(false) }}
-                            >
-                                CLIENT
-                            </Nav.Link>
-                            <Nav.Link
-                                as={Link}
-                                eventKey="contact-1"
-                                to={category === 'contact' ? `/` : `/?category=${'contact'}`}
-                                    onClick={() => { setExpanded(false) }}
-                            >
-                                INFO
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    )
-}
+      </Navbar>
+    </div>
+  );
+};
 
 export default Menu;
