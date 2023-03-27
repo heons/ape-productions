78f82a3597950e1824338f10be7e5e5e70ea72c6
HUEN_OH <heons921@gmail.com>
2023-03-27 07:52:45 -0400

    update dvds list

--- src/resources/dvds.js
+++ src/resources/dvds.js
@@ -1,53 +1,62 @@
 export const dvdList = [
-    {
-        title: `Super Korean DVD / Halloween, Iteawon, Soap with Juneone, Kibum, DJs`,
-        url: `https://www.youtube.com/embed/vGPUet3acW8`,
-        ratio: `16:9`,
-    },
-    {
-        title: `Dirty Rose Club DVD 2nd Party`,
-        url: `https://www.youtube.com/embed/51f2rZP39Vo`,
-        ratio: `16:9`,
-    },
-    {
-        title: `8BALLTOWN DVD / CASACORONA, ITAEWON, SEOUL, KOREA`,
-        url: `https://www.youtube.com/embed/Lgs30rngTjo`,
-        ratio: `16:9`,
-    },
-    {
-        title: `HENZ DVD / HONGDAE, SEOUL, KOREA`,
-        url: `https://www.youtube.com/embed/s_0OFoaYcOM`,
-        ratio: `16:9`,
-    },
-    {
-        title: `ALTER EGO : 21C RAVE DVD / CAKESHOP, SEOUL, KOREA`,
-        url: `https://www.youtube.com/embed/jvEs9nialmA`,
-        ratio: `16:9`,
-    },
-    {
-        title: `SOAPSEOUL DVD : MRSHALL LIVE PERFORMENCE / SEOUL, KOREA`,
-        url: `https://www.youtube.com/embed/L7ie5Q_BI8g`,
-        ratio: `16:9`,
-    },
-    {
-        title: `UMF KOREA DVD : UNDER WORLD / VIEWSIC IN GANGNAM`,
-        url: `https://www.youtube.com/embed/YFYsjxZGMbg`,
-        ratio: `16:9`,
-    },
-    {
-        title: `HOLLYDAY DVD : SABURINA, TORI MORI & CO KR, JUNEONE`,
-        url: `https://www.youtube.com/embed/brGRQ30xME8`,
-        ratio: `16:9`,
-    },
-    {
-        title: `GRAY - 하기나 해 JUST DO IT (Feat.Loco)👌 (Unofficial DVD)`,
-        url: `https://www.youtube.com/embed/L0k63-BCOBM`,
-        ratio: `16:9`,
-    },
-    {
-        title: `1 SHOT 1 OPPORTUNITY DVD 🤮 (Director's Cut)`,
-        url: `https://www.youtube.com/embed/BQEq_znFPtQ`,
-        ratio: `16:9`,
-    },
+  {
+    title: `👁ZICO LIVE with 99% at LEGACYSEOUL DVD💙💙💙`,
+    url: `https://www.youtube.com/embed/GdQpPIR60xI`,
+    ratio: `16:9`,
+  },
+  {
+    title: `👐🏽 AOMG Live at Henz Club in Hong-dae 🫀`,
+    url: `https://www.youtube.com/embed/A_K23MyhFag`,
+    ratio: `16:9`,
+  },
+  {
+    title: `Super Korean DVD / Halloween, Iteawon, Soap with Juneone, Kibum, DJs`,
+    url: `https://www.youtube.com/embed/vGPUet3acW8`,
+    ratio: `16:9`,
+  },
+  {
+    title: `Dirty Rose Club DVD 2nd Party`,
+    url: `https://www.youtube.com/embed/51f2rZP39Vo`,
+    ratio: `16:9`,
+  },
+  {
+    title: `8BALLTOWN DVD / CASACORONA, ITAEWON, SEOUL, KOREA`,
+    url: `https://www.youtube.com/embed/Lgs30rngTjo`,
+    ratio: `16:9`,
+  },
+  {
+    title: `HENZ DVD / HONGDAE, SEOUL, KOREA`,
+    url: `https://www.youtube.com/embed/s_0OFoaYcOM`,
+    ratio: `16:9`,
+  },
+  {
+    title: `ALTER EGO : 21C RAVE DVD / CAKESHOP, SEOUL, KOREA`,
+    url: `https://www.youtube.com/embed/jvEs9nialmA`,
+    ratio: `16:9`,
+  },
+  {
+    title: `SOAPSEOUL DVD : MRSHALL LIVE PERFORMENCE / SEOUL, KOREA`,
+    url: `https://www.youtube.com/embed/L7ie5Q_BI8g`,
+    ratio: `16:9`,
+  },
+  {
+    title: `UMF KOREA DVD : UNDER WORLD / VIEWSIC IN GANGNAM`,
+    url: `https://www.youtube.com/embed/YFYsjxZGMbg`,
+    ratio: `16:9`,
+  },
+  {
+    title: `HOLLYDAY DVD : SABURINA, TORI MORI & CO KR, JUNEONE`,
+    url: `https://www.youtube.com/embed/brGRQ30xME8`,
+    ratio: `16:9`,
+  },
+  {
+    title: `GRAY - 하기나 해 JUST DO IT (Feat.Loco)👌 (Unofficial DVD)`,
+    url: `https://www.youtube.com/embed/L0k63-BCOBM`,
+    ratio: `16:9`,
+  },
+  {
+    title: `1 SHOT 1 OPPORTUNITY DVD 🤮 (Director's Cut)`,
+    url: `https://www.youtube.com/embed/BQEq_znFPtQ`,
+    ratio: `16:9`,
+  },
 ];
-
